175cee07ee5ed9570a161c68546119c7e7ef124f
toheedhamid <toheedhamid9@gmail.com>
2025-09-29 10:41:05 +0500

    Initial commit

---
+++ my_django_project/my_django_project/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def analyze(request):
+    # Get the text
+    djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    uppercase = request.GET.get('uppercase', 'off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif (uppercase == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'covert to upper case', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    else:
+        return HttpResponse('Error')
+
+
